bd841e418aad3bbd9169c6e8168527a2093d1e49
Muzikaa <ajordaninlimbo@gmail.com>
2025-08-03 22:46:00 -0700

    Added some unversioned files, prepped for deployment.

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import auth from "auth-astro";
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+      plugins: [tailwindcss()]
+    },
+    output: "server",
+    base: "diaries",
+    site: 'https://muzikaa.github.io'
+});
